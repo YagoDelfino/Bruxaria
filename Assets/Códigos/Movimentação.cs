1829cbdf8a8b1cfcff9ce82bc2b042e736ef826d
YagoDelfino <daventhidas@gmail.com>
2019-08-13 18:14:59 -0300

    Initial commit

---
+++ Assets/Códigos/Movimentação.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentação : MonoBehaviour
+{
+
+
+    Vector2 Movimento;
+
+    public float Velocidade = 5f;
+
+    public Rigidbody2D rb;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimento.x = Input.GetAxisRaw("Horizontal");
+        Movimento.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Movimento * Velocidade * Time.fixedDeltaTime);
+    }
+}
